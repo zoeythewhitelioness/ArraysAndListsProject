a5c9d1dd4adc13f3ff0309409f2131f5bfb2d3bf
Zoey Baker <zoeybaker625@gmail.com>
2022-06-16 14:46:50 -0500

    initial commit

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿
+
+using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
     class Program
     {
+     
+
         static void Main(string[] args)
         {
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            var numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+
+            var evens = new List<int>();
+            var odds = new List<int>();
+
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -22,11 +33,68 @@ namespace ArraysAndLists
              * or the odds List
              */
 
+            for(int i = 0; i < numbers.Length; i++)
+
+            {
+
+                if (numbers[i] % 2 == 0)
+                {
+
+                    evens.Add(numbers[i]);
+
+                }
+                else
+                {
+
+                    odds.Add(numbers[i]);
+                }
+
+            }
+
+
+
+            foreach (var num in numbers)
+            {
+
+                if (num % 2 == 0)
+                {
+
+                    evens.Add(num);
+                }
+                else
+
+                {
+                    odds.Add(num);
+                }
+
+            }
+
+
+
             /* Now using foeach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+
+
+            foreach (var item in evens) 
+            {
+
+
+                Console.WriteLine($"{item} is an even number");
+
+            }
+            foreach (var oddNumber in odds)
+
+            {
+
+                Console.WriteLine($"{oddNumber} is def odd");
+            }
+
         }
+   
+
     }
 }
+
